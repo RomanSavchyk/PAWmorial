4b04c7c3aa69deed2f9c5cc13c29026bf04cc75f
Roman Savchuk <roman.sav15@gmail.com>
2019-08-18 22:28:48 +0300

    final

--- js/index.js
+++ js/index.js
@@ -1,15 +1,11 @@
-// $('.slider').slick({
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   arrows: false,
-//   fade: true,
-//   asNavFor: '.slider-nav',
-// });
-
-// $('.slider-nav').slick({
-//   slidesToShow: 2,
-//   slidesToScroll: 1,
-//   asNavFor: '.slider',
-//   dots: false,
-//   focusOnSelect: true
-// });
+$('.main-image').slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  slidesToShow: 1,
+  adaptiveHeight: false,
+  fade: true,
+  cssEase: 'linear'
+});
