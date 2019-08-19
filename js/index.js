4ba8794035eb336137bad0917e7e70765773bb2d
Roman Savchuk <roman.sav15@gmail.com>
2019-08-19 21:25:57 +0300

    reworked header nav

--- js/index.js
+++ js/index.js
@@ -1,11 +1,32 @@
-// $('.main-image').slick({
-//   dots: false,
-//   arrows: false,
-//   infinite: true,
-//   autoplay: true,
-//   autoplaySpeed: 5000,
-//   slidesToShow: 1,
-//   adaptiveHeight: false,
-//   fade: true,
-//   cssEase: 'linear'
-// });
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  autoplay: true,
+  autospeed: 300,
+  dots: false,
+  arrows: true,
+  prevArrow: '<i class="fa fa-angle-left prev1"></i>',
+  nextArrow: '<i class="fa fa-angle-left next1"></i>',
+  focusOnSelect: true
+});
+$('a[data-slide]').click(function(e) {
+  e.preventDefault();
+  var slideno = $(this).data('slide');
+  $('.slider-nav').slick('slickGoTo', slideno - 1);
+});
+
+const hamburger = $('.hamburger')
+const headerNav = $('.header-nav')
+
+hamburger.click(function() {
+  hamburger.toggleClass('hamburger__active')
+  headerNav.toggleClass('header-nav__active')
+})
