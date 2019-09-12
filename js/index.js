b4d870e945360c4d66b00b9e2b87d824e5b35a38
Roman Savchuk <roman.sav15@gmail.com>
2019-09-12 14:29:17 +0300

    testing height of input on mobile

--- js/index.js
+++ js/index.js
@@ -361,9 +361,11 @@ $(document).ready(function() {
     });
   });
 
-  $('#pet-sex').change(
-    function () {
-    $('#pet-sex').css("background", $("select option:selected").css("background-color"));
+  $(function() {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    $("html").css({"width":w,"height":h});
+    $("body").css({"width":w,"height":h});
   });
   
 })
