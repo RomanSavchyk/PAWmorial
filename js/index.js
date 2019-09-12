138c4554bedd20e94690e789cc7865e6ac34a4d7
Roman Savchuk <roman.sav15@gmail.com>
2019-09-12 15:17:02 +0300

    testing height of input on mobile

--- js/index.js
+++ js/index.js
@@ -361,12 +361,12 @@ $(document).ready(function() {
     });
   });
 
-  $(function() {
-    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    $("html").css({"width":w,"height":h});
-    $("body").css({"width":w,"height":h});
-  });
+  // $(function() {
+  //   var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  //   var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  //   $("html").css({"width":w,"height":h});
+  //   $("body").css({"width":w,"height":h});
+  // });
   
 })
 
